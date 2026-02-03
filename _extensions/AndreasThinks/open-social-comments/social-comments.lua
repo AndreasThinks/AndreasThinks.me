729b532051a25b27164e13a10a15c242af4f95f7
Andreas Varotsis <andreas.varotsis@gmail.com>
2026-02-03 20:27:54 +0000

    Revert "Merge pull request #25 from AndreasThinks/claude/fix-extension-formatting-M9sCu"
    This reverts commit 9e267e41838dbc77133d419af95b3403725869a0, reversing
    changes made to be35ad83c11352aa1505812e01e33304a2547c58.

--- _extensions/AndreasThinks/open-social-comments/social-comments.lua
+++ _extensions/AndreasThinks/open-social-comments/social-comments.lua
@@ -8,41 +8,36 @@ end
 
 function Meta(m)
   ensureHtmlDeps()
-
+  
   -- Initialize variables for both platforms
   local has_comments = false
   local social_html = '<social-comments'
-
-  -- Handle Mastodon configuration via data attributes
+  local js_vars = '<script type="text/javascript">\n'
+  
+  -- Handle Mastodon configuration
   if m.mastodon_comments and m.mastodon_comments.user and m.mastodon_comments.toot_id and m.mastodon_comments.host then
       local user = pandoc.utils.stringify(m.mastodon_comments.user)
       local toot_id = pandoc.utils.stringify(m.mastodon_comments.toot_id)
       local host = pandoc.utils.stringify(m.mastodon_comments.host)
-
-      social_html = social_html .. ' data-mastodon-host="' .. host .. '"'
-      social_html = social_html .. ' data-mastodon-user="' .. user .. '"'
-      social_html = social_html .. ' data-mastodon-toot-id="' .. toot_id .. '"'
-
+      
+      js_vars = js_vars ..
+      'var mastodonHost = "' .. host .. '";\n' ..
+      'var mastodonUser = "' .. user .. '";\n' ..
+      'var mastodonTootId = "' .. toot_id .. '";\n'
+      
       has_comments = true
   end
-
+  
   -- Handle Bluesky configuration
   if m.bluesky_comments and m.bluesky_comments.post_uri then
       local post_uri = pandoc.utils.stringify(m.bluesky_comments.post_uri)
       social_html = social_html .. ' bluesky-post="' .. post_uri .. '"'
       has_comments = true
   end
-
-  -- Handle hide_author_replies option via data attribute
-  if m.social_comments_options and m.social_comments_options.hide_author_replies then
-      local hide_author_replies = pandoc.utils.stringify(m.social_comments_options.hide_author_replies)
-      if hide_author_replies == "true" then
-          social_html = social_html .. ' data-hide-author-replies="true"'
-      end
-  end
-
+  
   social_html = social_html .. '></social-comments>'
-
+  js_vars = js_vars .. '</script>'
+  
   if has_comments then
       -- JavaScript to inject social comments into a specific div
       local inject_script = [[
@@ -60,6 +55,6 @@ document.addEventListener('DOMContentLoaded', function() {
       local script_html = '<script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.4.1/purify.min.js" integrity="sha512-uHOKtSfJWScGmyyFr2O2+efpDx2nhwHU2v7MVeptzZoiC7bdF6Ny/CmZhN2AwIK1oCFiVQQ5DA/L9FSzyPNu6Q==" crossorigin="anonymous"></script>'
 
       -- Insert these elements in the document's head
-      quarto.doc.includeText("in-header", script_html .. inject_script)
+      quarto.doc.includeText("in-header", script_html .. inject_script .. js_vars)
   end
 end
